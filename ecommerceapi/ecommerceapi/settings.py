b9ca1dd1e022af327184412cdce24941e7aa74b0
As Kalanza <kalanzavictor@gmail.com>
2025-12-02 23:53:07 +0300

    Add custom user model with phone_number and is_verified fields

--- ecommerceapi/ecommerceapi/settings.py
+++ ecommerceapi/ecommerceapi/settings.py
@@ -39,7 +39,11 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "ecomapi",
+    "users"
 ]
+
+# Custom User Model
+AUTH_USER_MODEL = 'users.CustomUser'
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
